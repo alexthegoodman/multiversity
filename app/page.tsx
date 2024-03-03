eef89016e447bfb1953513cc492a3753a60c37e5
Alex Goodman <alexthegoodman@gmail.com>
2024-03-03 18:36:52 -0500

    init home

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,51 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+
+import TextareaAutosize from "react-textarea-autosize";
+
+import shared from "./shared.module.scss";
+import styles from "./page.module.scss";
+
+const examplePrompts = [
+  "FFmpeg C++ Development",
+  "Isomorphic JavaScript",
+  "Introduction to Circuits",
+  "Advanced Data Structures",
+  "Assembly Code",
+  "Embedded Rust Programming",
+  "Autodesk's Fusion 360",
+  "Apple 1",
+];
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
+      <section className={styles.hero}>
+        <div className={styles.heroInner}>
+          <span>Multiversity</span>
+          <h1>Learn anything you can imagine, affordably</h1>
           <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            Enter the name of a course you want to take, or try one of the
+            courses shown below.
           </p>
-        </a>
-      </div>
+          <div className={styles.glowWrap}>
+            <TextareaAutosize
+              minRows={4}
+              placeholder="What do you want to learn about?"
+            />
+          </div>
+          <button className={shared.btn}>Start Learning</button>
+          <div className={styles.examplePrompts}>
+            <p>Many of these courses are hard to find or expensive elsewhere</p>
+            <ul>
+              {examplePrompts.map((prompt) => (
+                <li key={prompt}>
+                  <button className={shared.smallBtn}>{prompt}</button>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
